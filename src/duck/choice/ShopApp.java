850d51553699b41bc98776f78e2308fac3edddb2
emanoelcampos <emanoel.cv@gmail.com>
2023-06-06 08:57:40 -0300

    instantiated the clothing class and calculating the total

--- src/duck/choice/ShopApp.java
+++ src/duck/choice/ShopApp.java
@@ -4,9 +4,29 @@ public class ShopApp {
 
     public static void main(String[] args) {
 
-        Customer c1 = new Customer();
-        c1.name = "Pink";
+        double tax = 0.2;
+        double total;
 
-        System.out.println("Welcome to the Duck Shop " + c1.name + "!");
+        Customer customer1 = new Customer();
+        customer1.name = "Pink";
+
+        System.out.println("Welcome to the Duck Shop " + customer1.name + "!");
+
+        Clothing item1 = new Clothing();
+        Clothing item2 = new Clothing();
+
+        item1.description = "Blue Jacket";
+        item1.price = 20.9;
+        item1.size = "M";
+
+        item2.description = "Orange T-Shirt";
+        item2.price = 10.5;
+        item2.size = "S";
+
+        System.out.println("Item 1, " + item1.description + ", " + item1.price + " and " + item1.size);
+        System.out.println("Item 2, " + item2.description + ", " + item2.price + " and " + item2.size);
+
+        total = (2 * item2.price + item1.price) * (1 + tax);
+        System.out.println("Total: " + total);
     }
 }
